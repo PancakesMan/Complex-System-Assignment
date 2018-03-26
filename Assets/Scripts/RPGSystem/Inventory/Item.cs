15e062ce388920d6e31c39948d2aef0e5ac99509
Eric Vuglar <s171764@students.aie.edu.au>
2018-03-27 10:17:42 +1030

    Moved scripts

---
+++ Assets/Scripts/RPGSystem/Inventory/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGSystem
+{
+    [CreateAssetMenu(fileName = "New Item", menuName = "RPG System/Item")]
+    public class Item : ScriptableObject
+    {
+        public ItemTypes    itemType = ItemTypes.Type1;
+        public Sprite       itemSprite;
+    }
+}
