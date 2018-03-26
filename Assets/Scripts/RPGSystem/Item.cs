5e24c2b6bf71308bc815416a0635d5573661e579
Eric Vuglar <s171764@students.aie.edu.au>
2018-03-27 09:51:49 +1030

    ItemTypes enum and Item ScriptableObejct

---
+++ Assets/Scripts/RPGSystem/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGSystem
+{
+    public class Item : ScriptableObject
+    {
+        public ItemTypes    itemType;
+        public Sprite       itemSprite;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/RPGSystem/ItemTypes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGSystem
+{
+    public enum ItemTypes
+    {
+        Type1 = 1,
+        Type2 = 2,
+        Type3 = 4,
+        Type4 = 8,
+        Type5 = 16,
+        Type6 = 32,
+        Type7 = 64,
+        Type8 = 128,
+        Type9 = 256,
+        Type10 = 512,
+        Type11 = 1024,
+        Type12 = 2048,
+        Type13 = 4096,
+        Type14 = 8192,
+        Type15 = 16384,
+        Type16 = 32768,
+        Type17 = 65536,
+        Type18 = 131072,
+        Type19 = 262144,
+        Type20 = 524288,
+        Type21 = 1048576,
+        Type22 = 2097152,
+        Type23 = 4194304,
+        Type24 = 8388608,
+        Type25 = 16777216,
+        Type26 = 33554432,
+        Type27 = 67108864,
+        Type28 = 134217728,
+        Type29 = 268435456,
+        Type30 = 536870912,
+        Type31 = 1073741824,
+        All = 2147483647
+    }
+}
