8f87c74a29f7f51ea4844f279abe87a701243bcc
Eric Vuglar <s171764@students.aie.edu.au>
2018-03-27 12:30:03 +1030

    Item Type Editor

---
+++ Assets/Scripts/ItemTypeEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RPGSystem
+{
+    public class ItemTypeEditor : EditorWindow
+    {
+        Dictionary<string, int> _enum = new Dictionary<string, int>();
+        string enumTypeName;
+        int enumTypeValue;
+
+        [MenuItem("Window/Item Types")]
+        public static void ShowWindow()
+        {
+            GetWindow<ItemTypeEditor>("Item Types");
+        }
+        private void OnGUI()
+        {
+            GUILayout.Space(10);
+
+            foreach (KeyValuePair<string, int> value in _enum)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(20);
+                EditorGUILayout.TextField(value.Key, GUILayout.Width(150));
+                EditorGUILayout.IntField(value.Value, GUILayout.Width(150));
+                EditorGUILayout.EndHorizontal();
+            }
+
+            GUILayout.Space(10);
+
+            EditorGUILayout.BeginHorizontal();
+            enumTypeName = EditorGUILayout.TextField("", enumTypeName, GUILayout.Width(150));
+            enumTypeValue = EditorGUILayout.IntField("Bit Mask", enumTypeValue); 
+            if (GUILayout.Button("+"))
+            {
+                try
+                {
+                    _enum.Add(enumTypeName, enumTypeValue);
+                }
+                catch (System.Exception ex)
+                {
+
+                }   
+            }
+            EditorGUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+            if (GUILayout.Button("Update"))
+            {
+                System.IO.StreamWriter writer = new System.IO.StreamWriter("Assets/Scripts/RPGSystem/Items/ItemTypes.cs");
+                writer.WriteLine("namespace RPGSystem");
+                writer.WriteLine("{");
+                writer.WriteLine("\tpublic enum ItemTypes");
+                writer.WriteLine("\t{");
+                foreach (KeyValuePair<string, int> value in _enum)
+                    writer.WriteLine("\t\t" + value.Key + " = " + value.Value + ",");
+                writer.WriteLine("\t}");
+                writer.WriteLine("}");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RPGSystem/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGSystem {
+    public class Inventory : MonoBehaviour {
+
+        public int Width = 1;
+        public int Height = 1;
+
+        public int Size
+        {
+            private set { }
+            get {
+                return Width * Height;
+            }
+        }
+
+        Item[,] items;
+
+        // Use this for initialization
+        void Start() {
+            items = new Item[Width, Height];
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
