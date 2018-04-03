2e2691321dcc23051b85282cd04c200dd4ee31c1
Eric Vuglar <s171764@students.aie.edu.au>
2018-04-03 14:45:26 +0930

    inventory displays and resizes to fit contents

--- Assets/Scripts/RPGSystem/Inventory.cs
+++ Assets/Scripts/RPGSystem/Inventory.cs
@@ -14,6 +14,16 @@ namespace RPGSystem {
         public Item[,] Items;
         public Image SampleCell;
 
+        public bool Visible
+        {
+            private set { }
+            get {
+                if (inventory != null)
+                    return inventory.enabled;
+                return false;
+            }
+        }
+
         public int Size
         {
             private set { }
@@ -22,14 +32,42 @@ namespace RPGSystem {
             }
         }
 
+        private Image inventory;
+
         // Use this for initialization
         void Start() {
+
             Items = new Item[Width, Height];
+            inventory = SampleCell.transform.parent.GetComponent<Image>();
+            if (inventory != null)
+            {
+                //Resize inventory to fit cells
+                inventory.rectTransform.sizeDelta = new Vector2(
+                    (CellPadding * (Width + 1)) + (SampleCell.rectTransform.rect.width * Width),
+                    (CellPadding * (Height + 1)) + (SampleCell.rectTransform.rect.height * Height)
+                );
+
+                // Add item cells to the inventory
+                for (int x = 0; x < Width; x++)
+                {
+                    for (int y = 0; y < Height; y++)
+                    {
+                        Image nextCell = Instantiate(SampleCell);
+                        //nextCell.transform.parent = inventory.transform;
+                        nextCell.transform.SetParent(inventory.transform, true);
+                        Vector3 oldPos = SampleCell.transform.position;
+                        nextCell.transform.position= new Vector3(
+                            oldPos.x + SampleCell.rectTransform.rect.width * x + CellPadding * x,
+                            oldPos.y - SampleCell.rectTransform.rect.height * y - CellPadding * y
+                        );
+                    }
+                }
+            }
         }
 
         // Update is called once per frame
         void Update() {
-
+            
         }
 
         public bool AddItem(Item item)
@@ -67,6 +105,12 @@ namespace RPGSystem {
             // If there are no empty slots in the inventory
             // the item can't be added to it.
             return false;
+        }
+
+        public void Toggle()
+        {
+            if (inventory != null)
+                inventory.enabled = !inventory.enabled;
         }
     }
 }
