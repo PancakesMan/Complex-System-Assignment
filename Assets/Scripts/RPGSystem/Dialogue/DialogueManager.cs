f10530bc6ca2c0dc8801cabe3a7b9e4e9761ed2c
PancakesMan <so_awesome12@hotmail.com>
2018-05-05 19:23:58 +0930

    Changed DialogueManager to have Previous/Next buttons for Dialogue

--- Assets/Scripts/RPGSystem/Dialogue/DialogueManager.cs
+++ Assets/Scripts/RPGSystem/Dialogue/DialogueManager.cs
@@ -8,15 +8,23 @@ namespace RPGSystem
     public class DialogueManager : MonoBehaviour
     {
         private Queue<string> sentences;
+        private Dialogue _dialogue;
+        private int index;
 
         public Image DialogueBox;
         public Text Name;
         public Text DialogueText;
+        public Button Previous, Next, Accept, Decline;
 
         // Use this for initialization
         void Start()
         {
             sentences = new Queue<string>();
+            if (Previous)
+                Previous.onClick.AddListener(DisplayPreviousSentence);
+
+            if (Next)
+                Next.onClick.AddListener(DisplayNextSentence);
         }
 
         public void SetDialogue(Dialogue dialogue)
@@ -25,11 +33,18 @@ namespace RPGSystem
             foreach (string sentence in dialogue.sentences)
                 sentences.Enqueue(sentence);
 
-            DialogueBox.gameObject.SetActive(true);
+            _dialogue = dialogue;
+            index = -1;
+
             DialogueBox.transform.SetAsLastSibling();
+            DialogueBox.gameObject.SetActive(true);
+
+            //Previous.gameObject.SetActive(false);
+            
             Time.timeScale = 0;
             Name.text = dialogue.name;
-            StartCoroutine(DisplaySentence(sentences.Dequeue()));
+            //StartCoroutine(DisplaySentence(sentences.Dequeue()));
+            DisplayNextSentence();
         }
 
         public void AdvanceDialogue()
@@ -46,6 +61,29 @@ namespace RPGSystem
             StartCoroutine(DisplaySentence(sentences.Dequeue()));
         }
 
+        public void DisplayNextSentence()
+        {
+            index++;
+            if (_dialogue.sentences.Length == index)
+            {
+                DialogueBox.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                return;
+            }
+
+            SetDialogueButtonsStates();
+            StopAllCoroutines();
+            StartCoroutine(DisplaySentence(_dialogue.sentences[index]));
+        }
+
+        public void DisplayPreviousSentence()
+        {
+            index--;
+            SetDialogueButtonsStates();
+            StopAllCoroutines();
+            StartCoroutine(DisplaySentence(_dialogue.sentences[index]));
+        }
+
         IEnumerator DisplaySentence(string sentence)
         {
             DialogueText.text = "";
@@ -54,6 +92,22 @@ namespace RPGSystem
                 DialogueText.text += letter;
                 yield return null;
             }
+        }
+
+        private void SetDialogueButtonsStates()
+        {
+            if (index == 0)
+                Previous.gameObject.SetActive(false);
+            else
+                Previous.gameObject.SetActive(true);
+
+            if (_dialogue.sentences.Length - 1 == index)
+            {
+                // TODO check if Dialogue has a quest
+                Next.GetComponentInChildren<Text>().text = "Ok";
+            }
+            else
+                Next.GetComponentInChildren<Text>().text = "Next";
         }
     }
 }
