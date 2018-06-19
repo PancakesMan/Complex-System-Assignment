4c111ab9595ab732f2718a792636e00acc723e31
Eric Vuglar <s171764@students.aie.edu.au>
2018-06-19 11:31:45 +0930

    Procedural Trees. Glass Dice

---
+++ Assets/Scripts/Tree/Branch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch : MonoBehaviour {
+    public List<GameObject> branchEnds;
+    public List<GameObject> spawnableBranches;
+    public GameObject bush;
+
+    public int MinBranches = 5;
+    public int MaxBranches = 15;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject end in branchEnds)
+        {
+            int depth = 0;
+            Transform t = transform;
+            while (t != null)
+            {
+                depth++;
+                t = t.parent;
+            }
+
+            if (depth < Random.Range(MinBranches, MaxBranches))
+            {
+                GameObject obj = Instantiate(spawnableBranches[Random.Range(0, spawnableBranches.Count)], end.transform);
+                obj.transform.localScale = Vector3.one;
+                obj.transform.parent = end.transform;
+                obj.transform.Rotate(new Vector3(Random.Range(-40, 40), 0, Random.Range(-40, 40)));
+            }
+            else
+            {
+                GameObject obj = Instantiate(bush, end.transform);
+                //obj.transform.localScale = Vector3.one;
+                obj.transform.parent = end.transform;
+            }
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
